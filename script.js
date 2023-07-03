32b65d4d5a54c117b23b93aef90579dde65ed9c2
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-03 19:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
 //your JS code here. If required.
+const app = document.getElementById("app");
+const videoPlayer = document.getElementById("videoPlayer");
+const audioPlayer = document.getElementById("audioPlayer");
+const sound1Button = document.getElementById("sound1");
+const sound2Button = document.getElementById("sound2");
+const smallerMinsButton = document.getElementById("smaller-mins");
+const mediumMinsButton = document.getElementById("medium-mins");
+const longMinsButton = document.getElementById("long-mins");
+const timeDisplay = document.querySelector(".time-display");
+const playButton = document.querySelector(".play");
+
+let selectedSound = "Sounds/beach.mp3";
+let selectedVideo = "Videos/beach.mp4";
+
+sound1Button.addEventListener("click", () => {
+  selectedSound = "Sounds/beach.mp3";
+  selectedVideo = "Videos/beach.mp4";
+});
+
+sound2Button.addEventListener("click", () => {
+  selectedSound = "Sounds/rain.mp3";
+  selectedVideo = "Videos/rain.mp4";
+});
+
+function startTimer(duration) {
+  let timer = duration;
+  let minutes, seconds;
+
+  let countdown = setInterval(() => {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    timeDisplay.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      clearInterval(countdown);
+      timeDisplay.textContent = "Time's up!";
+    }
+  }, 1000);
+}
+
+smallerMinsButton.addEventListener("click", () => {
+  startTimer(120);
+});
+
+mediumMinsButton.addEventListener("click", () => {
+  startTimer(300);
+});
+
+longMinsButton.addEventListener("click", () => {
+  startTimer(600);
+});
+
+playButton.addEventListener("click", () => {
+  if (videoPlayer.paused && audioPlayer.paused) {
+    videoPlayer.src = selectedVideo;
+    audioPlayer.src = selectedSound;
+    videoPlayer.play();
+    audioPlayer.play();
+    playButton.textContent = "Pause";
+  } else {
+    videoPlayer.pause();
+    audioPlayer.pause();
+    playButton.textContent = "Play";
+  }
+});
